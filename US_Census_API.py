cce97753ce8af71ac49f9967e2f2a7b4674bed67
Brie <briecrafty@gmail.com>
2025-04-28 08:35:18 -0500

    Updating file structure and setting up output and input var

--- US_Census_API.py
+++ US_Census_API.py
@@ -6,8 +6,20 @@ import snowflake.connector
 
 stage=None
 
-config_path=os.path.join(os.path.dirname(__file__),'config_file.json')
+#Setting up config file input
+config_path=os.path.join(os.path.dirname(__file__),'config','config_file.json')
 with open (config_path,'r') as config_file:
     config=json.load(config_file)
 
+# file output dir
+output_dir=os.path.join(os.path.dirname(__file__),'data')
+os.makedirs(output_dir,exist_ok=True)
+
+#Setting up URL & API Key 
 API_KEY=config['API_KEY']
+BASE_URL="https://api.census.gov/data/{year}/acs/acs5?"
+PARAMS=("get=NAME,B01001_001E,B01001_002E,B01001_026E,B02001_002E,B02001_003E," 
+"B02001_004E,B02001_005E,B02001_006E,B02001_007E,B02001_008E,B19013_001E"
+"&for=place:*&in=state:48" 
+"&key={key}")
+
