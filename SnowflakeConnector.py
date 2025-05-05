10c6579ef9394747c1ae84b1ae2fdc238d98e647
Brie <briecrafty@gmail.com>
2025-05-05 10:24:29 -0500

    Seperating logic in SnowflakeConnector

--- SnowflakeConnector.py
+++ SnowflakeConnector.py
@@ -1,29 +1,44 @@
 
-import snowflake.connector
 #Creating a snowflake connection
+import snowflake.connector
 
-def create_snowflake_connection(config):
-    print(f"Connecting to Snowflake as user {config['snowflake_user']} on account {config['snowflake_account']}...")
-    global stage 
-    conn=snowflake.connector.connect(
-        user=config['snowflake_user'],
-        password=config['snowflake_password'],
-        account=config['snowflake_account'],
-        warehouse=config['snowflake_warehouse'],
-        schema=config['snowflake_schema'],
-        role=config['snowflake_role'],
-        database=config['snowflake_database'],
+class SnowflakeConnector:
+
+
+
+def _init_(self,config):
+    self.config=config;
+    self.conn=None;
+    self.stage=config['snowflake_stage']
+
+
+def create_snowflake_connection(self):
+    print(f"Connecting to Snowflake as user {self.config['snowflake_user']} on account {self.config['snowflake_account']}...")
+    self.conn=snowflake.connector.connect(
+        user=self.config['snowflake_user'],
+        password=self.config['snowflake_password'],
+        account=self.config['snowflake_account'],
+        warehouse=self.config['snowflake_warehouse'],
+        schema=self.config['snowflake_schema'],
+        role=self.config['snowflake_role'],
+        database=self.config['snowflake_database'],
         authenticator='snowflake'
     )
-    if conn is None:
+    if self.conn is None:
         print("Connection failed")
         exit()
     else:
         print("Connection Successful")
-    stage=config['snowflake_stage']
-    return conn
+    return self.conn
 
 #Uploading/Removing files to Snowflake stage
+
+#Removing files from Snowflake stage
+def remove_files_from_stage(self):
+    print(f"Removing all files from Snowflake stage {self.stage_name}...")
+
+
+
 
 def upload_files_to_stage(in_connection,filelist,stage_name):
     print(f"Removing all files from Snowflake stage {stage_name}...")
@@ -39,3 +54,4 @@ def upload_files_to_stage(in_connection,filelist,stage_name):
         cursor.execute(f"PUT file://{file} @{stage_name}")
         cursor.close()
         print(f"Upload of {file} to Snowflake stage {stage_name} complete.")
+
