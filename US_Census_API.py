7314fc6bf4aa2fa373a50988d5add39061498070
Brie <briecrafty@gmail.com>
2025-05-06 21:46:44 -0500

    Removing functions

--- US_Census_API.py
+++ US_Census_API.py
@@ -23,28 +23,6 @@ PARAMS=("get=NAME,B01001_001E,B01001_002E,B01001_026E,B02001_002E,B02001_003E,"
 "&for=place:*&in=state:48" 
 "&key={key}")
 
-#Creating a snowflake connection
-def create_snowflake_connection():
-    print(f"Connecting to Snowflake as user {config['snowflake_user']} on account {config['snowflake_account']}...")
-    global stage 
-    conn=snowflake.connector.connect(
-        user=config['snowflake_user'],
-        password=config['snowflake_password'],
-        account=config['snowflake_account'],
-        warehouse=config['snowflake_warehouse'],
-        schema=config['snowflake_schema'],
-        role=config['snowflake_role'],
-        database=config['snowflake_database'],
-        authenticator='snowflake'
-    )
-    if conn is None:
-        print("Connection failed")
-        exit()
-    else:
-        print("Connection Successful")
-    stage=config['snowflake_stage']
-    return conn
-
 #Get Census data from API
 def get_census_data (start_year, end_year):
     output_files=[]
@@ -66,22 +44,6 @@ def get_census_data (start_year, end_year):
            output_files.append(output_file)
         time.sleep(1)
     return output_files
-
-#Uploading/Removing files to Snowflake stage
-def upload_files_to_stage(in_connection,filelist,stage_name):
-    print(f"Removing all files from Snowflake stage {stage_name}...")
-    cursor=in_connection.cursor()
-    cursor.execute(f"REMOVE @{stage_name}")
-    cursor.close()
-    print(f"Removal of all files from Snowflake stage {stage_name} complete.")
-
-
-    for file in filelist:
-        print(f"Uploading {file} to Snowflake stage {stage_name}...")
-        cursor=in_connection.cursor()
-        cursor.execute(f"PUT file://{file} @{stage_name}")
-        cursor.close()
-        print(f"Upload of {file} to Snowflake stage {stage_name} complete.")
 
 if __name__=='__main__':
     start_year=2019
